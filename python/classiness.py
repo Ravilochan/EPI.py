a848987001db1999af3ca394b890fe02c82b2508
Ravilochan <ravilochan2000@gmail.com>
2023-09-03 17:28:37 +0530

    use dictonary instead of switch

--- python/classiness.py
+++ python/classiness.py
@@ -20,8 +20,13 @@ class Classy(object):
     def __init__(self):
         self.items = []
         self.classiness = 0
+        self.classiness_points = {
+            "tophat": 2,
+            "bowtie": 4,
+            "monocle": 5
+        }
 
-    def getPoints(self, item) -> int:
+    def get_points(self, item) -> int:
         match item:
             case "tophat":
                 return 2
@@ -32,30 +37,32 @@ class Classy(object):
             case default:
                 return 0
 
-    def getClassiness(self):
+    def get_classiness(self):
         return self.classiness
 
-    def addItem(self, item) -> None:
+    def add_item(self, item):
         self.items.append(item)
-        self.classiness += self.getPoints(item)
+        self.classiness += self.get_points(item)  # use get_points
+        # use dictionary
+        # self.classiness += self.classiness_points.get(item, 0)
 
 
 # Test cases
 me = Classy()
 
 # Should be 0
-print(me.getClassiness())
+print(me.get_classiness())
 
-me.addItem("tophat")
+me.add_item("tophat")
 # Should be 2
-print(me.getClassiness())
+print(me.get_classiness())
 
-me.addItem("bowtie")
-me.addItem("jacket")
-me.addItem("monocle")
+me.add_item("bowtie")
+me.add_item("jacket")
+me.add_item("monocle")
 # Should be 11
-print(me.getClassiness())
+print(me.get_classiness())
 
-me.addItem("bowtie")
+me.add_item("bowtie")
 # Should be 15
-print(me.getClassiness())
+print(me.get_classiness())
