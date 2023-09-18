fc2c6ddcbe7aff3c99bc989df5e792a8203ec9ea
Ravilochan <ravilochan2000@gmail.com>
2023-09-18 19:18:41 +0530

    in_order traversal of tree

--- epi_judge_python/tree_inorder.py
+++ epi_judge_python/tree_inorder.py
@@ -4,12 +4,38 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
-def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+# with recursive traversal
+def inorder_r_traversal(tree: BinaryTreeNode) -> List[int]:
+    def inorder(node: BinaryTreeNode, path: List[int]):
+        if node:
+            inorder(node.left, path)
+            path.append(node.data)
+            inorder(node.right, path)
+    order = []
+    inorder(tree, order)
+    return order
+
+
+# with iterative traversal
+def inorder_i_traversal(tree: BinaryTreeNode) -> List[int]:
+    stack, path = [], []
+    while stack or tree:
+        if tree:
+            stack.append(tree)
+            tree = tree.left
+        else:
+            tree = stack.pop()
+            path.append(tree.data)
+            tree = tree.right
+    return path
 
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('tree_inorder.py', 'tree_inorder.tsv',
-                                       inorder_traversal))
+                                       inorder_i_traversal))
+
+# if __name__ == '__main__':
+#     exit(
+#         generic_test.generic_test_main('tree_inorder.py', 'tree_inorder.tsv',
+#                                        inorder_r_traversal))
